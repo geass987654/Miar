86c373469031d8a49c50526705d6ca3fe64deb8c
geass987654 <geass987654@gmail.com>
2023-10-20 12:58:03 +0800

    更新人物移動動畫
    原先的動畫改為USBman的左右移動動畫

---
+++ Assets/Scripts/USBman.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class USBman : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject mybag;  //顯示的背包UI
+    private Animator animator;
+
+    [SerializeField] private float moveSpeed;
+    private float horizontal, vertical;
+    private Vector2 direction = Vector2.zero;
+    private bool isBagOpen = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        ReadPlayerInput();
+        SwitchAnim();
+        OpenBag();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ReadPlayerInput()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void SwitchAnim()
+    {
+        if (direction != Vector2.zero)
+        {
+            animator.SetFloat("horizontal", direction.x);
+            animator.SetFloat("vertical", direction.y);
+        }
+
+        animator.SetFloat("magnitude", direction.sqrMagnitude);
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + direction.normalized *  (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    void OpenBag()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            //當背包被打叉關掉，isBagOpen = false，簡化為以下程式碼
+            isBagOpen = mybag.activeSelf;
+
+            isBagOpen = !isBagOpen;
+            mybag.SetActive(isBagOpen);
+        }
+    }
+}
